1d9c53f0eb3ef3fbbd0503310a1a0c2832773cd2
JungWKim <lionmini@naver.com>
2022-08-14 20:26:13 +0900

    deleted previous cluster join

--- files/192.168.1.47/home/gpuadmin/k8s_join_worker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubeadm join 192.168.1.47:6443 --token ksuejd.p2oog8d4dv3lwde2 \
-    --discovery-token-ca-cert-hash sha256:faaba45a970eb1775216f133e25ca81a042104214ca02edcd3a8a23673a496d1 
