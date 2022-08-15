f320ae1ddee0592bff1f69985cd1137b481c8765
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-08-15 19:34:10 +0900

    Update create_kubeflow.sh

--- create_kubeflow.sh
+++ create_kubeflow.sh
@@ -5,7 +5,7 @@ echo -e "                              *                                        
 echo -e "                              *          install nfs_provisioner.yaml         *                              "
 echo -e "                              *                                               *                              "
 echo -e "                              *************************************************                              \n\n"
-#ansible-playbook install_nfs_provisioner.yaml -kK -e "admin=gpuadmin nfs_ip=192.168.1.123 nfs_path=/rancher"
+ansible-playbook install_nfs_provisioner.yaml -kK -e "admin=gpuadmin nfs_ip=192.168.1.123 nfs_path=/rancher"
 
 echo -e "                              *************************************************                              "
 echo -e "                              *                                               *                              "
