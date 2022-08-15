8fa9cdbdabe5295688a14b07809997d86d90f69f
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-08-15 21:10:23 +0900

    Update install_docker_ubuntu.sh

--- files/install_docker_ubuntu.sh
+++ files/install_docker_ubuntu.sh
@@ -8,7 +8,7 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt install -y docker-ce=19.03 docker-ce-cli=19.03 containerd.io docker-compose-plugin
 
 echo -e "\n\n\n------------------------------------------ docker images -----------------------------------------------"
 docker images
