9ecd20ab7edf024fdbffc9c69d85cf0351449704
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-08-16 19:51:52 +0900

    Update create_kubeflow.sh

--- create_kubeflow.sh
+++ create_kubeflow.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+admin=gpuadmin
+master_ip=192.168.1.47
+nfs_ip=192.168.1.123
+nfs_path=/rancher
+
 echo -e "                              *************************************************                              "
 echo -e "                              *                                               *                              "
 echo -e "                              *          install nfs_provisioner.yaml         *                              "
 echo -e "                              *                                               *                              "
 echo -e "                              *************************************************                              \n\n"
-ansible-playbook install_nfs_provisioner.yaml -kK -e "admin=gpuadmin nfs_ip=192.168.1.123 nfs_path=/rancher"
+ansible-playbook install_nfs_provisioner.yaml -kK -e "admin=${admin} nfs_ip=${nfs_ip} nfs_path=${nfs_path}"
 
 sleep 30
 
@@ -14,7 +19,7 @@ echo -e "                              *                                        
 echo -e "                              *            install kubeflow v1.5.yaml         *                              "
 echo -e "                              *                                               *                              "
 echo -e "                              *************************************************                              \n\n"
-ansible-playbook install_kubeflow_v1.5.yaml -kK -e "admin=gpuadmin master_ip=192.168.1.47"
+ansible-playbook install_kubeflow_v1.5.yaml -kK -e "admin=${admin} master_ip=${master_ip}"
 
 cd ~/manifests
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
