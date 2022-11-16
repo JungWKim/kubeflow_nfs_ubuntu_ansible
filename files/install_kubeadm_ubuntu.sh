f5a5489d7c78e31633fdc5fba45ccd29ed633387
JungWKim <lionmini@naver.com>
2022-11-17 00:18:12 +0900

    .

--- files/install_kubeadm_ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install -y kubelet=1.20.11-00 kubeadm=1.20.11-00 kubectl=1.20.11-00
-apt-mark hold kubelet kubeadm kubectl
